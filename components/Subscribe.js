38b257abf512c786b5037b280bf6cac830b96b7f
Tapas Adhikary <tapas.adhikary@gmail.com>
2021-08-27 17:13:49 +0530

    🐛 Mnor style fixes

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -34,11 +34,11 @@ const Subscribe = () => {
 
   return (
     <>  
-      <div className="border border-blue-200 rounded p-6 my-4 w-full dark:border-gray-800 bg-blue-50 dark:bg-blue-opaque">
-        <p className="text-lg md:text-xl font-bold text-gray-900 dark:text-gray-100">
+      <div className="border border-blue-200 rounded p-6 my-4 w-full bg-blue-50">
+        <p className="text-lg md:text-xl font-bold text-gray-900">
           Subscribe to the newsletter
         </p>
-        <p className="my-1 text-gray-800 dark:text-gray-200">
+        <p className="my-1 text-gray-800">
           Get emails from me about web development, content creation, and how lead a motivated life.
         </p>
         <form className="relative my-4" onSubmit={subscribeMe}>
@@ -49,27 +49,27 @@ const Subscribe = () => {
             type="email"
             autoComplete="email"
             required
-            className="px-4 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-gray-300 rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
+            className="px-4 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-gray-300 rounded-md bg-white text-gray-900"
           />
           <button
-            className="flex items-center justify-center absolute right-1 top-1 px-4 font-bold h-8 bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded w-28"
+            className="flex items-center justify-center absolute right-1 top-1 px-4 font-bold h-8 bg-gray-100 text-gray-900 rounded w-28"
             type="submit"
           >
             Subscribe
           </button>
         </form>
         {success 
-          ? <span className="flex items-center text-sm font-bold text-green-700 dark:text-green-400">{success}</span> 
-          : <span className="flex items-center text-sm font-bold text-red-800 dark:text-red-400">{error}</span>}
-        <p className="text-md text-gray-800 dark:text-gray-200">
+          ? <span className="flex items-center text-sm font-bold text-green-700">{success}</span> 
+          : <span className="flex items-center text-sm font-bold text-red-800">{error}</span>}
+        <p className="text-md text-gray-800">
           { subscriberCount } subscribers . {issues && issues.length} {issues && issues.length > 1 ? 'issues' : 'issue'}
         </p>
-        <p className="text-sm  text-center p-3 text-gray-800 dark:text-gray-200">
+        <p className="text-sm  text-center p-3 text-gray-800">
           Well, you can subscribe to my upcoming Newsletter for real. Just give a valid email id above and verify 😀.
         </p>
       </div>
       <div>
-        <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-8 text-black dark:text-white">
+        <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-8 text-black">
               Published Issues
         </h3>
         <div className="flex flex-col bg-gray-200 p-4 underline">
